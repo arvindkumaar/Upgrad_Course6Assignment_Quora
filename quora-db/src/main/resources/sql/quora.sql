3cfdfedbdd4a8f2f3ae236c6a98cc44abf3e9553
arvindkumaar <arvind.tomar@gmail.com>
2021-03-01 12:01:03 +0530

    Quora-DB work completed

--- quora-db/src/main/resources/sql/quora.sql
+++ quora-db/src/main/resources/sql/quora.sql
@@ -1,22 +1,21 @@
-
---USERS table is created to store the details of all the users
+-USERS table is created to store the details of all the users
 DROP TABLE IF EXISTS USERS CASCADE;
 CREATE TABLE IF NOT EXISTS USERS(id SERIAL, uuid VARCHAR(200) NOT NULL ,firstName VARCHAR(30) NOT NULL , lastName VARCHAR(30) NOT NULL ,userName VARCHAR(30) UNIQUE NOT NULL,  email VARCHAR(50) UNIQUE NOT NULL ,password VARCHAR(255) NOT NULL, salt VARCHAR(200) NOT NULL ,country VARCHAR(30) ,aboutMe VARCHAR(50),dob VARCHAR(30), role VARCHAR(30),contactNumber VARCHAR(30), PRIMARY KEY (id));
 INSERT INTO users(
-	id, uuid, firstname, lastname, username, email, password, salt, country, aboutme, dob, role, contactnumber)
-	VALUES (1024,'rdtrdtdyt','Abhi','Mahajan','abhi','a@gmail.com','507FF5FED1CAC746','8Xt6jxoCI3MWsVaKY/1ySAp2qzlb2Z7P89+vDrb1o6U=', 'India' ,'I am @ UpGrad' ,'22-10-1995' , 'admin' , '1222333333' );
+    id, uuid, firstname, lastname, username, email, password, salt, country, aboutme, dob, role, contactnumber)
+VALUES (1024,'rdtrdtdyt','Abhi','Mahajan','abhi','a@gmail.com','507FF5FED1CAC746','8Xt6jxoCI3MWsVaKY/1ySAp2qzlb2Z7P89+vDrb1o6U=', 'India' ,'I am @ UpGrad' ,'22-10-1995' , 'admin' , '1222333333' );
 
 --USER_AUTH table is created to store the login information of all the users
 DROP TABLE IF EXISTS USER_AUTH CASCADE;
 CREATE TABLE IF NOT EXISTS USER_AUTH(
-	ID BIGSERIAL PRIMARY KEY,
-	uuid VARCHAR(200) NOT NULL,
-	USER_ID INTEGER NOT NULL,
-	ACCESS_TOKEN VARCHAR(500) NOT NULL,
-	EXPIRES_AT TIMESTAMP NOT NULL,
-	LOGIN_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	LOGOUT_AT TIMESTAMP NULL
-);
+                                        ID BIGSERIAL PRIMARY KEY,
+                                        uuid VARCHAR(200) NOT NULL,
+    USER_ID INTEGER NOT NULL,
+    ACCESS_TOKEN VARCHAR(500) NOT NULL,
+    EXPIRES_AT TIMESTAMP NOT NULL,
+    LOGIN_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    LOGOUT_AT TIMESTAMP NULL
+    );
 
 ALTER TABLE USER_AUTH ADD CONSTRAINT FK_USER_AUTH_USER_ID FOREIGN KEY(USER_ID) REFERENCES USERS(ID) ON DELETE CASCADE ;
 
